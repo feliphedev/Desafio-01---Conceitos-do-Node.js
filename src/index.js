122e766b73aaef41d480e70cf1c0c705e33d4d8d
feliphedev <feliphe.dev@gmail.com>
2022-03-10 04:47:45 -0400

    get todos update

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ app.post('/users', (request, response) => {
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
   const { user } = request
-  return response.status(200).json({ todos: user.todos })
+  return response.status(200).json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
